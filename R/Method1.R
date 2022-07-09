2db6e4d9071216b298a37a1762a4ae68ba4e87b8
suthakaranr <64707100+suthakaranr@users.noreply.github.com>
2022-07-09 11:40:31 -0700

    Update Method1.R

--- R/Method1.R
+++ R/Method1.R
@@ -1,5 +1,6 @@
 Method1 = function(x){
   n = length(x)
+  k_PQ = delta_PQ = NULL
   All_Q = c(0.5, 0.6, 0.75, 0.85, n/(n + 1))
   for (i in 1:length(All_Q)) {
     Q = All_Q[i]
@@ -9,8 +10,8 @@ Method1 = function(x){
     n_2 = ceiling((n + 1) * Q)
     X_n1 = sort_x[n_1]
     X_n2 = sort_x[n_2]
-    k_PQ = log(X_n2/X_n1 - 1)/log(1 - P)
-    delta_PQ = ((X_n1)^2)/(2 * X_n1 - X_n2)
+    k_PQ[i] = log(X_n2/X_n1 - 1)/log(1 - P)
+    delta_PQ[i] = ((X_n1)^2)/(2 * X_n1 - X_n2)
   }
   sigma_PQ = k_PQ * delta_PQ
   k1hat0 = median(k_PQ)
